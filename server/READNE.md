982f53dc29366a4b3f59e775bef8cc7cd0c3930a
An Van Nguyen <anvnguy@gmail.com>
2023-12-05 17:43:18 +0700

    reset all project

---
+++ server/READNE.md
@@ -0,0 +1,7 @@
+## Lib:
+- Node.js - Nền tảng xây dựng back-end
+- Hyperledger Composer Node.js SDK - Thư viện hỗ trợ tương tác với hệ thống Hyperledger Fabric
+- JSONWebToken - Thư viện hỗ trợ tạo token bảo mật giữa client và server
+- Express.js - Thư viện hỗ trợ xây dựng các API của server
+- Twillio - Thư viện hỗ trợ xác thực số điện thoại người dùng
+- Mongoose - Thư viện hỗ trợ tổ chức các Schema, Model trên CSDL MongoDB
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const settingEnv = require('./config');
+const settingDB = require('./data/config/mongoose');
+const interfaces = require('./interfaces');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//Create server
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/', interfaces);
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server is listening at port ${process.env.PORT}`);
+});
+
+module.exports = { app };
